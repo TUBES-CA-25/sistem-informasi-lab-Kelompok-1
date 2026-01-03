a13847c39c6aa82d3d6a5ebd66d70e85e183e6c4
thalitashrl <thalitashrl@gmail.com>
2026-01-03 14:39:41 +0800

    bapak push test

--- app/config/database.php
+++ app/config/database.php
@@ -4,14 +4,14 @@
  */
 
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3310');
+define('DB_PORT', '3306');
 define('DB_NAME', 'iclabs');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_CHARSET', 'utf8mb4');
 
 /**
- * Get Database Connection
+ * bapak
  * @return PDO
  */
 function getDBConnection() {
